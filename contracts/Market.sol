6bb4283a5b2fe940b0b7bafbb4c387ee97535c5c
CarlosFaria94 <carlosfaria94@gmail.com>
2018-09-22 21:18:18 +0100

    Add acceptOffer, getOffer, getAcceptedOffer and withdrawNotAcceptedOffers to Market

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -1,6 +1,5 @@
 pragma solidity ^0.4.24;
 
-import './energy_token.sol';
 import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
 
 contract Market {
@@ -25,10 +24,6 @@ contract Market {
         Offer acceptedOffer;
         bool isEnergyDelivered;
     }
-     
-    enum State { Open, Close, Canceled }
-    enum Action { Buy, Sell }
-    enum Product { Day, Week, Month }
 
     struct Offer {
         uint256 id;
@@ -37,7 +32,17 @@ contract Market {
         uint256 value;
         bytes32 secretHash;
         uint256 unsafeCreatedTimestamp;
+        bool isWithdraw;
     }
+     
+    enum State { Open, Close, Canceled }
+    enum Action { Buy, Sell }
+    enum Product { Day, Week, Month }
+
+    event NewOrder(uint256 indexed orderId);
+    event OrderCanceled(uint256 indexed orderId);
+    event NewOffer(uint256 indexed orderId, uint256 indexed offerId);
+    event OfferAccepted(uint256 indexed orderId, uint256 indexed offerId);
 
     // Checks if the order exist.
     modifier orderExist(uint _orderId) {
@@ -60,6 +65,12 @@ contract Market {
     // Checks if the order is in Open state.
     modifier isOpen(uint _orderId) {
         require(orders[_orderId].state == uint(State.Open));
+        _;
+    }
+
+    // Checks if the order is in Close state.
+    modifier isClose(uint _orderId) {
+        require(orders[_orderId].state == uint(State.Close));
         _;
     }
 
@@ -96,6 +107,7 @@ contract Market {
         order.isEnergyDelivered = false;
 
         orders[orderCount] = order;
+        emit NewOrder(orderCount);
     }
 
     function getOrder(uint _orderId) 
@@ -136,13 +148,14 @@ contract Market {
         isOpen(_orderId)
     {
         orders[_orderId].state = uint(State.Canceled);
+        emit OrderCanceled(_orderId);
     }
 
-    function submitOffer(uint256 _oderId, uint256 _price, bytes32 _secretHash) 
+    function submitOffer(uint256 _orderId, uint256 _price, bytes32 _secretHash) 
         public payable 
         stop_if_emergency()
     {
-        Order storage order = orders[_oderId];
+        Order storage order = orders[_orderId];
         // Only possible to submit an Offer when an order state is Open
         require(order.state == uint(State.Open), "Order is not open to offers");
         order.offerCount = order.offerCount.add(1);
@@ -152,33 +165,89 @@ contract Market {
             _price,
             _secretHash,
             msg.value,
-            block.timestamp
+            block.timestamp,
+            false
         );
         order.offers[order.offerCount] = newOffer;
+        emit NewOffer(_orderId, order.offerCount);
     }
 
     function acceptOffer(uint256 _orderId, uint256 _offerId) 
         public
         stop_if_emergency()
+        orderExist(_orderId)
+        offerExist(_orderId, _offerId)
+        isOrderOwner(_orderId, msg.sender)
+        isOpen(_orderId)
     {
-        
+        Order storage order = orders[_orderId];
+        order.state = uint(State.Close);
+        order.acceptedOffer = order.offers[_offerId];
+        emit OfferAccepted(_orderId, _offerId);
     }
 
-    function getOffer(uint256 _orderId, uint256 _offerId) public view 
+    function getOffer(uint256 _orderId, uint256 _offerId) 
+        public view
+        orderExist(_orderId)
+        offerExist(_orderId, _offerId)
+        returns (uint, address, uint, uint, bytes32, uint, bool)
     {
-        
+        Offer storage offer = orders[_orderId].offers[_offerId];
+        return (
+            offer.id,
+            offer.owner,
+            offer.price,
+            offer.value,
+            offer.secretHash,
+            offer.unsafeCreatedTimestamp,
+            offer.isWithdraw
+        );
     }
 
-    function getAcceptedOffer(uint256 _orderId) public view
+    function getAcceptedOffer(uint256 _orderId) 
+        public view
+        orderExist(_orderId)
+        offerExist(_orderId, orders[_orderId].acceptedOffer.id)
+        returns (uint, address, uint, uint, bytes32, uint, bool)
     {
-        
+        Offer storage acceptedOffer = orders[_orderId].acceptedOffer;
+        return (
+            acceptedOffer.id,
+            acceptedOffer.owner,
+            acceptedOffer.price,
+            acceptedOffer.value,
+            acceptedOffer.secretHash,
+            acceptedOffer.unsafeCreatedTimestamp,
+            acceptedOffer.isWithdraw
+        );
     }
 
-    function claimPayment(uint256 _orderId, uint256 _offerId) 
+    function withdrawPayment(uint256 _orderId, uint256 _offerId) 
         public
         stop_if_emergency()
+        orderExist(_orderId)
+        offerExist(_orderId, _offerId)
+        isClose(_orderId)
     {
-        
+        // TODO: Only possible to withdraw payment to the producer, when presented the proof that energy was sent
+    }
+
+    /**
+    * @dev Remaining suppliers with non accepted offers should be able to withdraw their Ether
+    */
+    function withdrawNotAcceptedOffers(uint256 _orderId, uint256 _offerId) 
+        public
+        stop_if_emergency()
+        orderExist(_orderId)
+        offerExist(_orderId, _offerId)
+        isClose(_orderId)
+    {
+        Order storage order = orders[_orderId];
+        Offer offer = order.offers[_offerId];
+        require(msg.sender != order.acceptedOffer.owner, "Owner of the accepted offer cannot withdraw his funds with this function");
+        require(offer.isWithdraw == false, "Withdraw was already collected");
+        require(msg.sender == offer.owner, "Offer owner is the only one able to execute the withdraw");
+        offer.owner.transfer(offer.value);
     }
 
     /**
